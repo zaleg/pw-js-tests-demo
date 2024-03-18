a6dd81bdb20de5c0f108ac063f070b0474d24110
ozaporozhets <ozaporozhets@intellias.com>
2024-03-18 19:17:13 +0200

    headless turn on for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,8 +37,8 @@ export default defineConfig({
       name: 'chromium',
       use: 
         { ...devices['Desktop Chrome'], 
-          headless: false, 
-           launchOptions: { slowMo: 800 }
+          //headless: false, 
+          // launchOptions: { slowMo: 800 }
         },
     },
 
