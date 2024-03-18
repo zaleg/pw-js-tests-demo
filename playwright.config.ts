7af2415330140df7a5a1ae074b17198e0d889cb1
ozaporozhets <ozaporozhets@intellias.com>
2024-03-18 19:13:07 +0200

    add github actions ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
       use: 
         { ...devices['Desktop Chrome'], 
           headless: false, 
-          // launchOptions: { slowMo: 800 }
+           launchOptions: { slowMo: 800 }
         },
     },
 
